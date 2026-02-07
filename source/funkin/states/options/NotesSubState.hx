af72bd69377651a2b7198806d537235677b42c55
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2026-02-06 22:14:53 -0500

    ok

--- source/funkin/states/options/NotesSubState.hx
+++ source/funkin/states/options/NotesSubState.hx
@@ -327,7 +327,7 @@ class NotesSubState extends MusicBeatSubstate
 	}
 
 	function changeSelection(change:Int = 0) {
-		curSelected += FlxMath.wrap(curSelected + change, 0, FlxMath.minInt(valuesArray.length, namesArray.length) - 1);
+		curSelected += FlxMath.wrap(curSelected + change, 0, namesArray.length - 1);
 		curValue = valuesArray[curSelected][typeSelected];
 		updateValue();
 
