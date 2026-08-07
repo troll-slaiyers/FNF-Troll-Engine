3e932f4f681647d20bf88fca3adc20c379e9d427
riconuts <68708956+riconuts@users.noreply.github.com>
2026-08-07 04:24:52 -0400

    Update Sowy.hx

--- source/funkin/macros/Sowy.hx
+++ source/funkin/macros/Sowy.hx
@@ -21,6 +21,18 @@ class Sowy
 		return macro $v{haxe.macro.Context.getDefines()};
 	}
 
+	/**
+		Returns the contents of a file at compile time
+	**/
+	public static macro function readFile(path:String, throwException:Bool = true) {
+		if (!sys.FileSystem.exists(path)) {
+			if (throwException)
+				haxe.macro.Context.error('File "$path" does not exist', haxe.macro.Context.currentPos());
+			return macro $v{null};
+		}
+		return macro $v{sys.io.File.getContent(path)};
+	}
+
 	public static function findByName(fields:Array<Field>, name:String):Null<Field>{
 		for (field in fields){
 			if (field.name == name)
